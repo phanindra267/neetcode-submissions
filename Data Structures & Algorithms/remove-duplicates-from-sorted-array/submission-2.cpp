60656000ded35afd259f144fe42e554d29b9f35f
Kakumani Phaneendra <151868018+phanindra267@users.noreply.github.com>
2026-06-12 16:57:54 +0530

    Add: remove-duplicates-from-sorted-array - submission-2

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set<int> unique(nums.begin(),nums.end());
+        int i=0;
+        for(int num:unique)
+        {
+            nums[i++]=num;
+
+        } 
+        return unique.size();
+    
+
+
+    }
+};
